1314b362f97fe1b29d60df5cc34e51638fe9b636
MichelSc <michelschaffers@gmail.com>
2019-11-11 11:11:06 +0100

    First version of emf project

--- common.moplaf.solver/com.misc.common.moplaf2.solver/src/com/misc/common/moplaf2/solver/IFormulation.java
+++ common.moplaf.solver/com.misc.common.moplaf2.solver/src/com/misc/common/moplaf2/solver/IFormulation.java
@@ -1,23 +1,31 @@
 package com.misc.common.moplaf2.solver;
 
+// TODO
+// question: should we give the all path in the query 
+//   instead of the root element
+//   this would allow us to implement the interface with indexes
+//   optionally
+
 import java.util.Collection;
 
 public interface IFormulation extends Constants{
+	// tuple elements
 	Collection<?> getRootTuples();
 	Collection<?> getTupleSubTuples(Object tuple);
 	Collection<?> getTupleVariables(Object tuple);
 	Collection<?> getTupleConstraints(Object tuple);
 	
+	// var elements
 	String getVariableName(Object var);
 	int getVariableType(Object var);
 	double getVariableLowerBound(Object var);
 	double getVariableUpperBound(Object var);
 	
-	String getConstraintName(Object var);
-	
+	// cons elements
+	String getConstraintName(Object cons);
+	int getConstraintType(Object cons);
 	// from here on, specific to linear formulation
-	int getConstraintType(Object var);
-	double getConstraintRighHandSide(Object var);
+	double getConstraintRighHandSide(Object cons);
 	Collection<?> getLinearConstraintTerms(Object cons);
 	Object getTermVar(Object term);
 	double getTermCoefficient(Object term);
